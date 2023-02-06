236981e17a220ddbf5e10f8c83c1abf3ad13a76d
Nirupama Duddu <NXD0VZ5@fpl.com>
2023-02-06 12:30:18 +0530

    Adding Project to git

---
+++ AppRatingTest/AppRatingTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.StoreReview;
+using Xamarin.Forms;
+
+namespace AppRatingTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CrossStoreReview.Current.RequestReview(false);
+
+        }
+    }
+}
+
